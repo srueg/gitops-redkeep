9591743dfa7168b37034e19b84e6ac2fffda6ff4
Simon Rüegg <simon@rueggs.ch>
2020-01-19 14:19:41 +0100

    Search in all directories

--- rotate-sealed-secrets.sh
+++ rotate-sealed-secrets.sh
@@ -3,9 +3,9 @@
 set -o errexit
 set -o pipefail
 set -o nounset
-#set -o xtrace
+set -o xtrace
 
-for secret in $(grep -lR SealedSecret **/*.yaml)
+for secret in $(grep -lR --include \*.yaml SealedSecret .)
 do
     kubeseal \
       --controller-namespace sealed-secrets \
