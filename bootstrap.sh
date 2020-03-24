1fcc839e84a3c03524d5869eff116c3073a79347
Simon Rüegg <simon@rueggs.ch>
2020-03-24 21:56:54 +0100

    Bootstrap using Helm v2

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,16 +16,16 @@ export GIT_REPO='git@github.com:srueg/gitops-redkeep.git'
 kapply create namespace $TILLER_NAMESPACE
 kapply create clusterrolebinding tiller-admin --clusterrole cluster-admin --serviceaccount $TILLER_NAMESPACE:default
 
-helm init \
+helm2 init \
   --tiller-namespace $TILLER_NAMESPACE \
   --override 'spec.template.spec.containers[0].command'='{/tiller,--storage=secret}' \
   --history-max 10 \
   --wait
 
-helm repo add fluxcd https://charts.fluxcd.io
-helm repo update
+helm2 repo add fluxcd https://charts.fluxcd.io
+helm2 repo update
 
-helm upgrade -i flux fluxcd/flux \
+helm2 upgrade -i flux fluxcd/flux \
   --namespace $FLUX_FORWARD_NAMESPACE \
   --set git.url=$GIT_REPO \
   --set git.readonly=true \
@@ -36,7 +36,7 @@ if hash fluxctl 2>/dev/null; then
     fluxctl identity
 fi
 
-helm upgrade -i flux-helm-operator fluxcd/helm-operator \
+helm2 upgrade -i flux-helm-operator fluxcd/helm-operator \
   --version 0.3.0 \
   --namespace $FLUX_FORWARD_NAMESPACE \
   --set tillerNamespace=$TILLER_NAMESPACE
